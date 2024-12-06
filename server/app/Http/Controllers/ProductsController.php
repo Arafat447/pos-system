6bb0569b978e660179264bdc49fb473d198f8c65
Arafat447 <arafathossain05541@gmail.com>
2024-12-06 18:30:23 +0600

    update

---
+++ server/app/Http/Controllers/ProductsController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Products;
+use Illuminate\Http\Request;
+
+class ProductsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index() {
+        return Products::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request) {
+        $data = $request->validate([
+            'name' => 'required|string',
+            'price' => 'required|numeric',
+            'stock' => 'required|integer',
+            'trade_offer_min_qty' => 'nullable|string',
+            'trade_offer_get_qty' => 'nullable|string',
+            'discount' => 'nullable|numeric',
+            'discount_or_trade_offer_start_date' => 'nullable|date',
+            'discount_or_trade_offer_end_date' => 'nullable|date',
+        ]);
+
+        $product = Products::create($data);
+        return response()->json($product, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Products  $products
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Products $products)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Products  $products
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Products $products)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Products  $products
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Products $products)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Products  $products
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Products $products)
+    {
+        //
+    }
+}
---
+++ server/database/migrations/2024_12_06_122500_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->decimal('price', 10, 2);
+            $table->integer('stock');
+            $table->string('trade_offer_min_qty')->nullable();
+            $table->string('trade_offer_get_qty')->nullable();
+            $table->decimal('discount', 5, 2)->nullable();
+            $table->dateTime('discount_or_trade_offer_start_date')->nullable();
+            $table->dateTime('discount_or_trade_offer_end_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
